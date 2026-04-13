2247d4dd2511cf30432694df07c3025605683281
adinemohamed <adinemohamed202@gmail.com>
2026-04-13 16:57:35 +0200

    fix email smtp railway

--- server.js
+++ server.js
@@ -44,13 +44,15 @@ const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
 //////////////////////////////////////////////////////
-// EMAIL
+// EMAIL (🔥 تم التعديل هنا فقط)
 //////////////////////////////////////////////////////
 let transporter;
 
 try {
   transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
